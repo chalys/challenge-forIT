121dbc02a2c0bc81290f3980f441028a6bedb5da
Carlos Roberto Arando Llanos <chalys.all@gmail.com>
2025-03-24 22:47:37 -0400

    feat: Agregar controlador para listar tareas

--- backend/src/controllers/tasks/list.controller.js
+++ backend/src/controllers/tasks/list.controller.js
@@ -1,1 +1,15 @@
-module.exports = (req, res) => {console.log("listar")};
+const Task = require("../../db/models/task");
+
+const listTasks = async (req, res) => {
+  try {
+    const tasks = await Task.findAll({
+      order: [["createdAt", "DESC"]],
+    });
+    res.json(tasks);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Error al obtener las tareas' });
+  }
+};
+
+module.exports = listTasks;
